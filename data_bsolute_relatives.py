9bab880b45651b4f2181c598bcfde32d14d207fd
adilsonsimiaoDK <adilsonsimiaock@gmail.com>
2021-03-25 09:31:27 +0100

    Data absolute and relatives

--- data_bsolute_relatives.py
+++ data_bsolute_relatives.py
@@ -1,11 +1,13 @@
 import pandas as pd
 data= {'jobs': ['DBA','DEV', 'NetArq' ],
        'new_jersey':[97350, 82080,112840],
-       'florida': [77140, 71540, 622310]}
+       'florida': [77140, 71540, 62310]}
 dataset = pd.DataFrame(data)
-print(dataset)
+
 total_nj = dataset['new_jersey'].sum()
 total_fl = dataset['florida'].sum()
 dataset['percent_fl'] = dataset['florida']/ total_fl * 100
-dataset['percent_nj'] = dataset['']/ total_fl * 100
-
+dataset['percent_nj'] = dataset['new_jersey']/ total_nj * 100
+dataset['percent_nj'] =dataset['percent_nj'].astype(int)
+dataset['percent_fl'] =dataset['percent_fl'].astype(int)
+print(dataset)
